9177360e6df93c61069d235245305341e7d1df4c
Garrett van Wageningen <ghvanw283@gmail.com>
2021-01-15 16:51:18 -0600

    add test and console example projects

---
+++ examples/ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+Console.WriteLine("Hello Compass!");
+
+var path = @"C:\Users\ghvw\projects\csharp\compass\shapefile\supercool.shp";
+
+var bytes = await File.ReadAllBytesAsync(path);
+
+Console.WriteLine($"There are {bytes.Length} bytes in the shapefile");
