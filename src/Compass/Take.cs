317cff351b347132476d7c39485f623edc2bbe18
Garrett van Wageningen <ghvanw283@gmail.com>
2021-01-16 11:13:29 -0600

    update take to use immutable list for now

--- src/Compass/Take.cs
+++ src/Compass/Take.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Compass {
 
-    public class Take<A> : IParser<A> {
+    public class Take<A> : IParser<ImmutableList<A>> {
 
         private readonly int n;
         private readonly IParser<A> parser;
@@ -16,14 +17,22 @@ namespace Compass {
             this.parser = parser;
         }
 
-        public (A, ArraySegment<byte>)? Call(ArraySegment<byte> bytes) {
-            //Enumerable
-            //    .Range(0, this.n)
-            //    .Select(_ => this.parser)
-            //    .Aggregate(new Return<List<A>>> (new List<A>()), (result, p) => {
-
-            //    });
-            throw new NotImplementedException();
-        }
+        public (ImmutableList<A>, ArraySegment<byte>)? Call(ArraySegment<byte> bytes) =>
+            Enumerable
+                .Range(0, this.n)
+                .Select(_ => this.parser)
+                .Aggregate(new Return<ImmutableList<A>>(ImmutableList<A>.Empty) as IParser<ImmutableList<A>>, (result, p) => {
+                    return (from list in result
+                            from item in p
+                            select list.Add(item)); //works with ImmutableList but not List
+                    //return result.SelectMany(list => {
+                    //    return p.SelectMany(a => {
+                    //        list.Add(a);
+                    //        return new Return<List<A>>(list);
+                    //    });
+                    //});
+                })
+                .Call(bytes);
+            //throw new NotImplementedException();
     }
 }
