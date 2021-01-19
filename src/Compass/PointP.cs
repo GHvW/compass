4973b712e504225e06ac5526e9eeb87b773eefe4
Garrett van Wageningen <ghvanw283@gmail.com>
2021-01-18 21:53:53 -0600

    Create PointP.cs

---
+++ src/Compass/PointP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compass {
+
+    public class PointP : IParser<double> {
+
+        public PointP() {
+            this.parser = parser;
+        }
+
+        public (double, ArraySegment<byte>)? Call(ArraySegment<byte> bytes) {
+            throw new NotImplementedException();
+            
+        }
+    }
+}
